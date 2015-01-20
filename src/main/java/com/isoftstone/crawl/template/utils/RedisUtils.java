3d27a04edd6c54f84e5e261a0350e72ec8cb7d12
Lewis Liu <94660697@qq.com>
2015-01-20 07:06:23 +0000

    新加redis分库保存功能

--- src/main/java/com/isoftstone/crawl/template/utils/RedisUtils.java
+++ src/main/java/com/isoftstone/crawl/template/utils/RedisUtils.java
@@ -11,24 +11,24 @@ import com.isoftstone.crawl.template.impl.TemplateResult;
 public class RedisUtils {
 	private static JedisPool pool = null;
 	private static PropertiesUtils propert = PropertiesUtils.getInstance();
-	
+
 	public static JedisPool getPool() {
 		if (pool == null) {
 			JedisPoolConfig config = new JedisPoolConfig();
+
 			config.setMaxTotal(1024);
-			//最大能够保持idel状态的对象数  
+			// 最大能够保持idel状态的对象数
 			config.setMaxIdle(500);
-			//当池内没有返回对象时，最大等待时间  
+			// 当池内没有返回对象时，最大等待时间
 			config.setMaxWaitMillis(1000 * 100);
-			//当调用borrow Object方法时，是否进行有效性检查  
+			// 当调用borrow Object方法时，是否进行有效性检查
 			config.setTestOnBorrow(true);
-		
+
 			String ip = Constants.REDIS_IP;
 			if (propert.getValue("template.redis.ip") != null)
 				ip = propert.getValue("template.redis.ip");
 
 			int port = Constants.REDIS_PORT;
-		
 			if (propert.getValue("template.redis.port") != null)
 				port = Integer.parseInt(propert.getValue("template.redis.port"));
 
@@ -43,7 +43,7 @@ public class RedisUtils {
 		}
 	}
 
-	public static TemplateResult getTemplateResult(String guid) {
+/*	public static TemplateResult getTemplateResult(String guid) {
 		JedisPool pool = null;
 		Jedis jedis = null;
 		try {
@@ -59,9 +59,29 @@ public class RedisUtils {
 			returnResource(pool, jedis);
 		}
 		return null;
+	}*/
+
+	public static TemplateResult getTemplateResult(String guid, int dbindex) {
+		JedisPool pool = null;
+		Jedis jedis = null;
+		try {
+			pool = getPool();
+			jedis = pool.getResource();
+			jedis.select(dbindex);
+			String json = jedis.get(guid);
+			if (json != null)
+				return JSONUtils.getTemplateResultObject(json);
+		} catch (Exception e) {
+			pool.returnBrokenResource(jedis);
+			e.printStackTrace();
+		} finally {
+			returnResource(pool, jedis);
+		}
+		return null;
 	}
 
-	public static void setTemplateResult(TemplateResult templateResult, String guid) {
+	
+/*	public static void setTemplateResult(TemplateResult templateResult, String guid) {
 		JedisPool pool = null;
 		Jedis jedis = null;
 		try {
@@ -76,9 +96,43 @@ public class RedisUtils {
 		} finally {
 			returnResource(pool, jedis);
 		}
+	}*/
+	
+	public static void setTemplateResult(TemplateResult templateResult, String guid, int dbindex) {
+		JedisPool pool = null;
+		Jedis jedis = null;
+		try {
+			StringBuilder str = new StringBuilder();
+			str.append(JSONUtils.getTemplateResultJSON(templateResult));
+			pool = getPool();
+			jedis = pool.getResource();
+			jedis.select(dbindex);
+			jedis.set(guid, str.toString());
+		} catch (Exception e) {
+			pool.returnBrokenResource(jedis);
+			e.printStackTrace();
+		} finally {
+			returnResource(pool, jedis);
+		}
 	}
 
-	public static long remove(String guid) {
+	public static void saveStr(String str, String guid, int dbindex) {
+		JedisPool pool = null;
+		Jedis jedis = null;
+		try {
+			pool = getPool();
+			jedis = pool.getResource();
+			jedis.select(dbindex);
+			jedis.set(guid, str);
+		} catch (Exception e) {
+			pool.returnBrokenResource(jedis);
+			e.printStackTrace();
+		} finally {
+			returnResource(pool, jedis);
+		}
+	}
+
+/*	public static long remove(String guid) {
 		JedisPool pool = null;
 		Jedis jedis = null;
 		try {
@@ -92,18 +146,36 @@ public class RedisUtils {
 			returnResource(pool, jedis);
 		}
 		return -1;
+	}*/
+
+	public static long remove(String guid, int dbindex) {
+		JedisPool pool = null;
+		Jedis jedis = null;
+		try {
+			pool = getPool();
+			jedis = pool.getResource();
+			jedis.select(dbindex);
+			return jedis.del(guid);
+		} catch (Exception e) {
+			pool.returnBrokenResource(jedis);
+			e.printStackTrace();
+		} finally {
+			returnResource(pool, jedis);
+		}
+		return -1;
 	}
 
-	public static ParseResult getParseResult(String guid) {
+	public static ParseResult getParseResult(String guid, int dbindex) {
 
 		JedisPool pool = null;
 		Jedis jedis = null;
 		try {
 			pool = getPool();
 			jedis = pool.getResource();
-			//System.out.println("guid=" + guid);
+			jedis.select(dbindex);
+			// System.out.println("guid=" + guid);
 			String json = jedis.get(guid);
-			//System.out.println("json=" + json);
+			// System.out.println("json=" + json);
 			return JSONUtils.getParseResultObject(json);
 		} catch (Exception e) {
 			pool.returnBrokenResource(jedis);
@@ -114,7 +186,7 @@ public class RedisUtils {
 		return null;
 	}
 
-	public static void setParseResult(ParseResult parseResult, String guid) {
+	public static void setParseResult(ParseResult parseResult, String guid, int dbindex) {
 		JedisPool pool = null;
 		Jedis jedis = null;
 		try {
@@ -122,6 +194,7 @@ public class RedisUtils {
 			str.append(JSONUtils.getParseResultJSON(parseResult));
 			pool = getPool();
 			jedis = pool.getResource();
+			jedis.select(dbindex);
 			jedis.set(guid, str.toString());
 		} catch (Exception e) {
 			pool.returnBrokenResource(jedis);
@@ -131,13 +204,31 @@ public class RedisUtils {
 		}
 	}
 
-	public static boolean contains(String guid) {
+/*	public static boolean contains(String guid) {
 		JedisPool pool = null;
 		Jedis jedis = null;
 		boolean flag = false;
 		try {
 			pool = getPool();
 			jedis = pool.getResource();
+			flag = jedis.exists(guid);
+		} catch (Exception e) {
+			pool.returnBrokenResource(jedis);
+			e.printStackTrace();
+		} finally {
+			returnResource(pool, jedis);
+		}
+		return flag;
+	}*/
+
+	public static boolean contains(String guid, int dbindex) {
+		JedisPool pool = null;
+		Jedis jedis = null;
+		boolean flag = false;
+		try {
+			pool = getPool();
+			jedis = pool.getResource();
+			jedis.select(dbindex);
 			flag = jedis.exists(guid);
 		} catch (Exception e) {
 			pool.returnBrokenResource(jedis);
