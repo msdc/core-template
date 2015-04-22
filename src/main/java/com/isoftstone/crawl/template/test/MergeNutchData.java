2d62370dda6e11039154a309c3b7311bcd6d7654
Donegal Chen <murdercdh@hotmail.com>
2015-04-22 08:36:52 +0000

    add MergeNutchData

---
+++ src/main/java/com/isoftstone/crawl/template/test/MergeNutchData.java
@@ -0,0 +1,11 @@
+package com.isoftstone.crawl.template.test;
+
+/**
+ * Created by Administrator on 2015/4/22.
+ */
+public class MergeNutchData {
+    public static void main(String[] args) {
+
+    }
+
+}
